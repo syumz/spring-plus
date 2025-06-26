3768110ba3107148a9b4cc107a3f763ed9ffb38b
seoyeon <dlatjdus0303@naver.com>
2025-06-26 13:55:38 +0900

    레벨 1-3: 코드 개선 - JPA의 이해

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
@@ -47,10 +49,10 @@ public class TodoService {
     }
 
     @Transactional(readOnly = true)
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime startTime, LocalDateTime endTime) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findTodosWithUserByWeatherAndModifiedAtDesc(pageable, weather, startTime, endTime);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
