b4acbb104c758fa58c9307c97cbe3ab2bd59072a
seoyeon <dlatjdus0303@naver.com>
2025-06-27 20:25:55 +0900

    레벨 2-8: QueryDSL

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -5,18 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryQuery {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u WHERE (:weather IS NULL OR t.weather = :weather) AND (:startTime IS NULL OR t.modifiedAt >= :startTime) AND (:endTime IS NULL OR t.modifiedAt <= :endTime) ORDER BY t.modifiedAt DESC")
     Page<Todo> findTodosWithUserByWeatherAndModifiedAtDesc(Pageable pageable, String weather, LocalDateTime startTime, LocalDateTime endTime);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
