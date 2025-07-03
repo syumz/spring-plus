9c1d498df30f13f7c3795143a0ad35e0aa5b875b
seoyeon <dlatjdus0303@naver.com>
2025-07-03 20:43:23 +0900

    add: application.properties와 gradle-wrapper.properties 추가

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.expert;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExpertApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.example.expert;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ExpertApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
